26b07a9155da0d3a3198cd4063e7fafd2723e1ba
Tuan Hung <>
2026-01-19 14:28:01 +0700

    - Adjust life system

--- Assets/Life System/Scripts/LifeDataHolder.cs
+++ Assets/Life System/Scripts/LifeDataHolder.cs
@@ -1,18 +1,11 @@
 using System.Collections.Generic;
 
-//Classes to Stote current life progress and time to add next life
 namespace TechJuego.LifeSystem
 {
     [System.Serializable]
-    public class AddTimeClass
+    public class LifeData
     {
         public int CurrentLifeCount;
         public List<string> AddedNextTime = new List<string>();
     }
-    [System.Serializable]
-    public class LifeData
-    {
-        public AddTimeClass lifedata = new AddTimeClass();
-    }
-
 }
--- Assets/Life System/Scripts/LifeHandler.cs
+++ Assets/Life System/Scripts/LifeHandler.cs
@@ -11,12 +11,14 @@ namespace TechJuego.LifeSystem
     public class LifeHandler : MonoBehaviour
     {
         public static LifeHandler Instance;
-        private readonly string lifeHolder = "LIFE";
-        private LifeData lifeData = new LifeData();
         [Header("Max no of life")]
         [SerializeField] private int MaxLifeCount;
         [Header("Time in seconds")]
         [SerializeField] private int TimeToAddLifeInSeconds;
+        
+        private readonly string LifeDataKey = "LIFE";
+        private LifeData lifeData = new LifeData();
+        
         private void Awake()
         {
             if (Instance == null)
@@ -28,65 +30,65 @@ namespace TechJuego.LifeSystem
             {
                 Destroy(gameObject);
             }
-            if (PlayerPrefs.HasKey(lifeHolder))
+            if (PlayerPrefs.HasKey(this.LifeDataKey))
             {
-                lifeData = JsonUtility.FromJson<LifeData>(PlayerPrefs.GetString(lifeHolder));
+                lifeData = JsonUtility.FromJson<LifeData>(PlayerPrefs.GetString(this.LifeDataKey));
             }
             else
             {
-                lifeData.lifedata.CurrentLifeCount = MaxLifeCount;
-                PlayerPrefs.SetString(lifeHolder, JsonUtility.ToJson(lifeData));
+                lifeData.CurrentLifeCount = MaxLifeCount;
+                PlayerPrefs.SetString(this.LifeDataKey, JsonUtility.ToJson(lifeData));
             }
             CheckLife();
         }
-        //function to reduce life count and check it don't go below 0
+
         public void LooseLife()
         {
-            if (lifeData.lifedata.CurrentLifeCount > 0)
+            if (lifeData.CurrentLifeCount > 0)
             {
-                lifeData.lifedata.CurrentLifeCount -= 1;
-                PlayerPrefs.SetString(lifeHolder, JsonUtility.ToJson(lifeData));
+                lifeData.CurrentLifeCount -= 1;
+                PlayerPrefs.SetString(this.LifeDataKey, JsonUtility.ToJson(lifeData));
                 SetTimeToAddNextLife();
             }
         }
-        //function to add life count and check it don't go above max count
+        
         public void AddLife()
         {
-            if (lifeData.lifedata.CurrentLifeCount < MaxLifeCount)
+            if (lifeData.CurrentLifeCount < MaxLifeCount)
             {
-                lifeData.lifedata.CurrentLifeCount += 1;
-                PlayerPrefs.SetString(lifeHolder, JsonUtility.ToJson(lifeData));
+                lifeData.CurrentLifeCount += 1;
+                PlayerPrefs.SetString(this.LifeDataKey, JsonUtility.ToJson(lifeData));
             }
         }
-        //Refill all lifes
+        
         public void RefillLife()
         {
-            lifeData.lifedata.CurrentLifeCount = MaxLifeCount;
-            lifeData.lifedata.AddedNextTime = new System.Collections.Generic.List<string>();
-            PlayerPrefs.SetString(lifeHolder, JsonUtility.ToJson(lifeData));
+            lifeData.CurrentLifeCount = MaxLifeCount;
+            lifeData.AddedNextTime = new System.Collections.Generic.List<string>();
+            PlayerPrefs.SetString(this.LifeDataKey, JsonUtility.ToJson(lifeData));
         }
-        // update remaining time
+        
         private void Update()
         {
-            if (lifeData.lifedata.CurrentLifeCount < MaxLifeCount)
+            if (lifeData.CurrentLifeCount < MaxLifeCount)
             {
-                if (lifeData.lifedata.AddedNextTime.Count > 0)
+                if (lifeData.AddedNextTime.Count > 0)
                 {
-                    TimeSpan span = DateTime.Parse(lifeData.lifedata.AddedNextTime[0]) - DateTime.Now;
-                    LifeEvents.OnGetLifeDetail?.Invoke(lifeData.lifedata.CurrentLifeCount, GetRemainingTime(span));
+                    TimeSpan span = DateTime.Parse(lifeData.AddedNextTime[0]) - DateTime.Now;
+                    LifeEvents.OnGetLifeDetail?.Invoke(lifeData.CurrentLifeCount, GetRemainingTime(span));
                     if (span.TotalSeconds < 0)
                     {
-                        lifeData.lifedata.AddedNextTime.RemoveAt(0);
+                        lifeData.AddedNextTime.RemoveAt(0);
                         AddLife();
                     }
                 }
             }
             else
             {
-                LifeEvents.OnGetLifeDetail?.Invoke(lifeData.lifedata.CurrentLifeCount, "Full");
+                LifeEvents.OnGetLifeDetail?.Invoke(lifeData.CurrentLifeCount, "Full");
             }
         }
-        // get time in string format to show
+        
         private string GetRemainingTime(TimeSpan timeSpan)
         {
             string time = "";
@@ -100,41 +102,41 @@ namespace TechJuego.LifeSystem
             }
             return time;
         }
-        //function to check is if we have max life
+        
         private bool IsLifeIsFull()
         {
-            return lifeData.lifedata.CurrentLifeCount >= MaxLifeCount;
+            return lifeData.CurrentLifeCount >= MaxLifeCount;
         }
-        //function to check before can we play
+        
         public bool CanPlay()
         {
-            return lifeData.lifedata.CurrentLifeCount > 0;
+            return lifeData.CurrentLifeCount > 0;
         }
-        //function to set time to add next life
+        
         void SetTimeToAddNextLife()
         {
             var seconds = TimeToAddLifeInSeconds;
-            if (lifeData.lifedata.AddedNextTime.Count > 0)
+            if (lifeData.AddedNextTime.Count > 0)
             {
-                string times = lifeData.lifedata.AddedNextTime[lifeData.lifedata.AddedNextTime.Count - 1];
+                string times = lifeData.AddedNextTime[lifeData.AddedNextTime.Count - 1];
                 DateTime nextTime = DateTime.Parse(times).AddSeconds(seconds);
-                lifeData.lifedata.AddedNextTime.Add(nextTime.ToString());
+                lifeData.AddedNextTime.Add(nextTime.ToString());
             }
             else
             {
                 DateTime nextTime = DateTime.Now.AddSeconds(seconds);
-                lifeData.lifedata.AddedNextTime.Add(nextTime.ToString());
+                lifeData.AddedNextTime.Add(nextTime.ToString());
             }
-            PlayerPrefs.SetString(lifeHolder, JsonUtility.ToJson(lifeData));
+            PlayerPrefs.SetString(this.LifeDataKey, JsonUtility.ToJson(lifeData));
         }
-        //function to check on start 
+        
         void CheckLife()
         {
-            if (lifeData.lifedata.AddedNextTime.Count > 0)
+            if (lifeData.AddedNextTime.Count > 0)
             {
-                string times = lifeData.lifedata.AddedNextTime[lifeData.lifedata.AddedNextTime.Count - 1];
+                string times = lifeData.AddedNextTime[lifeData.AddedNextTime.Count - 1];
                 TimeSpan span = DateTime.Parse(times) - DateTime.Now;
-                LifeEvents.OnGetLifeDetail?.Invoke(lifeData.lifedata.CurrentLifeCount, GetRemainingTime(span));
+                LifeEvents.OnGetLifeDetail?.Invoke(lifeData.CurrentLifeCount, GetRemainingTime(span));
             }
         }
     }
